322342d2c2a53015a6f4d9407ce51298be996a3e
Matias Rietig <rietig.m@googlemail.com>
2020-03-30 12:02:40 +0200

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+    }
+
+    // performed before physics calculation
+    void FixedUpdate() {
+        float steering = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(0, 0, steering);
+        rb.AddForce(movement * 50);
+    }
+}
